3c347165bc277f5ba55186203fd1d71b5c90ad7c
HARSHIT SHARMA <harshit.sharma.ums@gmail.com>
2024-05-13 22:36:32 +0530

    fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,11 +8,11 @@ export function Home() {
            
         <div className=" lg:flex text-center justify-center  font-mono" >
             
-           <div className="lg:w-1/2  bg-cover xs:96 bg-center sl:bg-cover md:bg-cover  xs:bg-cover  bg-gray-100 h-screen w-screen   p-8 space-y-8  justify-center xs:bg-none 
+           <div className="lg:w-1/2  bg-cover bg-fit xs:px-56     bg-center sl:bg-cover md:bg-cover  xs:bg-cover  bg-gray-100 h-screen w-screen   p-8 space-y-8  justify-center xs:bg-none 
             bg-no-repeat items-center"  style={{backgroundImage:`url(${dnsimage3})`}} > 
         </div>
            
-            <div className="lg:w-3/5   bg-amber-200 ">
+            <div className="lg:w-3/5 lg:max-h-screen bg-cover xs:justify-center  bg-amber-200 ">
                 <div className="flex flex-cols lg:h-screen sticky  justify-center items-center">
                     <Credentails />
                 </div>
